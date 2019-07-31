b84be12726d5c7f3c19b4cbd39628acdad37b48c
Vitaly Platonov <39029044+adancedwin@users.noreply.github.com>
2019-08-01 00:35:26 +0300

    Eliminate the need to pass a player object and add a check

--- models/player.rb
+++ models/player.rb
@@ -2,8 +2,9 @@ class Player < ApplicationRecord
   belongs_to :team
   has_many :player_results
   
-  def self.achieved_result_in_last_matches?(player, result, matches = 5)
-    last_matches = player.team.matches.order(id: :desc).limit(matches).map(&:id)
+  def achieved_result_in_last_matches?(result, matches = 5)
+    return unless self.team.matches
+    last_matches = self.team.matches.order(id: :desc).limit(matches).map(&:id)
     player.player_results.where('match_id = ? AND result = ?', last_matches, result).any?
   end
 end
